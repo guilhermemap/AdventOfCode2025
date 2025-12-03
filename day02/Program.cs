8dac9b4e15b620077731276397c1420eb0dfd478
guilherme <guilherme@acheipneus.com.br>
2025-12-03 20:39:04 -0300

    day 02 part 2

--- day02/Program.cs
+++ day02/Program.cs
@@ -22,23 +22,60 @@ long SumOfInvalidIds(long start, long end)
     var sum = 0L;
     for (var id = start; id < end; id++)
     {
-        var str = id.ToString();
-        var len = str.Length;
-        if (len % 2 != 0) continue;
-        var ok = true;
-        var halfLen = len / 2;
-        for (var i = 0; i < halfLen; i++)
-        {
-            if (str[i] == str[i + halfLen]) continue;
-            ok = false;
-            break;
-        }
-
-        if (ok)
+        if (IsInvalidIdRecursive(id))
         {
             sum += id;
         }
     }
 
     return sum;
+}
+
+bool IsInvalidId(long id)
+{
+    var str = id.ToString();
+    var len = str.Length;
+    if (len % 2 != 0) return false;
+    var ok = true;
+    var halfLen = len / 2;
+    for (var i = 0; i < halfLen; i++)
+    {
+        if (str[i] == str[i + halfLen]) continue;
+        ok = false;
+        break;
+    }
+
+    return ok;
+}
+
+bool IsInvalidIdRecursive(long id)
+{
+    var str = id.ToString();
+    var len = str.Length;
+    var halfLen = len / 2;
+    for (var i = 1; i <= halfLen; i++)
+    {
+        if (IsRepetition(str, i))
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+bool IsRepetition(string str, int bit)
+{
+    var totalLength = str.Length;
+    if (totalLength % bit != 0) return false;
+    var firstBit = str[..bit];
+    for (var i = bit; i < totalLength; i += bit)
+    {
+        if (str.Substring(i, bit) != firstBit)
+        {
+            return false;
+        }
+    }
+
+    return true;
 }
