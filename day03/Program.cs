8f4ed2952bfc91f9523c935bd6b42bc02659a3d6
guilherme <guilherme@acheipneus.com.br>
2025-12-03 16:46:48 -0300

    day 03 part 1

---
+++ day03/Program.cs
@@ -0,0 +1,30 @@
+﻿var lines = File.ReadAllLines("input");
+var total = 0;
+foreach (var line in lines)
+{
+    // there must be a second digit, so the last one can't be the first
+    var (firstDigit, firstPosition) = GetLargestDigit(line[..^1]);
+    var (secondDigit, secondPosition) = GetLargestDigit(line[(firstPosition + 1)..]);
+    var output = int.Parse($"{line[firstPosition]}{line[secondPosition + firstPosition + 1]}");
+    Console.WriteLine($"{line} => {output}");
+    total += output;
+}
+
+Console.WriteLine(total);
+return;
+
+(int largest, int position) GetLargestDigit(string input)
+{
+    var largest = 0;
+    var position = 0;
+    for (var i = 0; i < input.Length; i++)
+    {
+        var ch = input[i];
+        var digit = ch - '0';
+        if (digit <= largest) continue;
+        largest = digit;
+        position = i;
+    }
+
+    return (largest, position);
+}
