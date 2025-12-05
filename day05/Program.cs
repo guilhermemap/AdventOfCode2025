61488128c7f0d418a2992d6434b48fb7422801c9
guilherme <guilherme@acheipneus.com.br>
2025-12-05 14:29:40 -0300

    day05 part1

---
+++ day05/Program.cs
@@ -0,0 +1,45 @@
+﻿var lines = File.ReadAllLines("input");
+List<(long first, long last)> ranges = [];
+List<long> ingredients = [];
+var readingRanges = true;
+foreach (var line in lines)
+{
+    if (readingRanges)
+    {
+        if (string.IsNullOrWhiteSpace(line))
+        {
+            readingRanges = false;
+            continue;
+        }
+
+        var splits = line.Split('-');
+        var first = long.Parse(splits[0]);
+        var last = long.Parse(splits[1]);
+        ranges.Add((first, last));
+    }
+    else // reading ingredient now
+    {
+        ingredients.Add(long.Parse(line));
+    }
+}
+
+var totalFresh = 0;
+foreach (var ingredient in ingredients)
+{
+    var fresh = false;
+    foreach (var range in ranges)
+    {
+        if (ingredient >= range.first && ingredient <= range.last)
+        {
+            fresh = true;
+            break;
+        }
+    }
+
+    if (fresh)
+    {
+        totalFresh++;
+    }
+}
+
+Console.WriteLine($"Part one: {totalFresh}");
